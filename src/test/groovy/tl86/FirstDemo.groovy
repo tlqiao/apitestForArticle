92eda55841dd1a83a69aaacf36e73c241ca46d9e
tlqiao <qiaotl@163.com>
2019-04-17 19:46:00 +0800

    add two case

---
+++ src/test/groovy/tl86/FirstDemo.groovy
@@ -0,0 +1,19 @@
+package tl86
+
+import spock.lang.Specification
+
+import static io.restassured.RestAssured.given
+
+class FirstDemo extends Specification {
+
+    def "should call mock api successfully"() {
+        given: "no given"
+        when: "call mock api api"
+        given().baseUri("http://localhost:9090")
+                .when()
+                .get("api/getUserDetails")
+                .then()
+                .assertThat().statusCode(200)
+        then: "no then"
+    }
+}
